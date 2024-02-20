38fd2efd292216858724c5aba94f363c96567b35
Vishnu Priyan S S <vishnu32510@gmail.com>
2024-02-20 16:40:30 -0600

    Initial Commit

---
+++ GitHubSwift/GitHubSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSwiftApp.swift
+//  GitHubSwift
+//
+//  Created by Vishnu Priyan Sellam Shanmugavel on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
